b30b0bfa910f547713b2e78bd922b9e85ced73ca
chloe0307 <chloecuny@yahoo.fr>
2020-11-09 15:37:25 +0100

    modifications style

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,6 +3,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
 
+   
     const url = 'http://api.weatherstack.com/current?access_key=da731f3b4e97742d2660fa33cc1c370a&query=' + latitude + ',' + longitude + '&units=m'
 //  ici on a utilisé la syntaxe abrégée ES6 : url : url => url, pas de changement dans json:true car la propriété et la valeur n'ont pas le même nom. 
 //  et on a changé response en { body } car on sait que dans body est dans l'objet response, on fait directement appel a body. le résultat est le même
@@ -13,7 +14,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'Aujourd\'hui le temps est ' + body.current.weather_descriptions + '. Il fait actuellement ' + body.current.temperature + ' degrés, et le risque de précipitations est de ' + body.current.precip + '%'
+            callback(undefined, 
+                'Aujourd\'hui le temps est ' + body.current.weather_descriptions[0] + '.' +
+                'Il fait actuellement ' + body.current.temperature + ' degrés.' + 
+                'Le risque de précipitations est de ' + body.current.precip + '%'
                 
                 // weather_descriptions: body.current.weather_descriptions,
                 // temperature: body.current.temperature,
