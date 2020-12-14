9c2237d6f59183a0cf8ce0edd1cb789220ab35ed
chloe0307 <chloecuny@yahoo.fr>
2020-12-14 22:21:59 +0100

    adding vercel.json file to deploy website on vercel

--- src/notifications-emails/account.js
+++ src/notifications-emails/account.js
@@ -1,1 +1,15 @@
-const sendGridAPIKey = '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'
+// This file using to send emails as notifications on using users account with Send Grid
+
+// NPM imports
+const sgMail = require('@sendgrid/mail')
+
+// API key on Send Grid
+const sendGridAPIKey = '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'
+
+sgMail.setApiKey(sendGridAPIKey)
+sgMail.send({
+    to : 'chloecuny@yahoo.fr',
+    from : 'chloecuny@yahoo.fr',
+    subject: 'This is my first creation',
+    text : 'I hope this one actually get to you'
+})
