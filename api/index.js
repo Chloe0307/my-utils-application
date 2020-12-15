c97ded9f579c05968cf9b6f4a79dae62d41e4dd4
chloe0307 <chloecuny@yahoo.fr>
2020-12-15 15:09:30 +0100

    try vercel deploy

--- api/index.js
+++ api/index.js
@@ -2,17 +2,17 @@
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
-require('./src/database/mongoose')
+require('../src/database/mongoose')
 
 
 // === FILES IMPORTS
 // Weather functions
-const geocode = require('./src/utils-functions/geocode')
-const forecast = require('./src/utils-functions/forecast')
+const geocode = require('../src/utils-functions/geocode')
+const forecast = require('../src/utils-functions/forecast')
 
 // Router
-const userRouter = require('./src/routers/users-router')
-const taskRouter = require('./src/routers/tasks-router')
+const userRouter = require('../src/routers/users-router')
+const taskRouter = require('../src/routers/tasks-router')
 
 // === Define express and port 
 const app = express()
@@ -34,7 +34,7 @@ hbs.registerPartials(partialsPath)
 
 // APP.USE méthods
 app.use(express.static(publicDirectoryPath))
-app.use('/static', express.static(path.join(__dirname + 'public')))
+// app.use('/static', express.static(path.join(__dirname + 'public')))
 //  la méthode .json() va nous permettre de transmettre les données json directement à un objet pour créer nos users
 app.use(express.json())
 // va nous permettre d'appeler le fichier qui contient nos routes pour nos utilisateurs ainsi que nos tâches.
